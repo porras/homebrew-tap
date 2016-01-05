c7acbb10324744d7d1480022a5eb515eff6bee9d
Sergio Gil <sgilperez@gmail.com>
2016-01-05 01:39:42 +0100

    [tlcr] Move bottle block before dependencies

--- tlcr.rb
+++ tlcr.rb
@@ -4,6 +4,11 @@ class Tlcr < Formula
   url "https://github.com/porras/tlcr/archive/0.1.1.tar.gz"
   sha256 "6ed81377a751b2679e2052be6e88b9b15a5feda59eb9bb024a1eefbee7b99295"
 
+  bottle do
+    root_url "https://github.com/porras/tlcr/releases/download/0.1.1"
+    sha256 "5c464967d69a51185abdb99ff23a39f659f37a510a89f0c27617f5c43bfa0772" => :yosemite
+  end
+
   depends_on "crystal-lang" => :build
   depends_on "openssl"
   depends_on "libevent"
@@ -19,10 +24,5 @@ class Tlcr < Formula
 
   test do
     system "tlcr", "--completion"
-  end
-
-  bottle do
-    root_url "https://github.com/porras/tlcr/releases/download/0.1.1"
-    sha256 "5c464967d69a51185abdb99ff23a39f659f37a510a89f0c27617f5c43bfa0772" => :yosemite
   end
 end
