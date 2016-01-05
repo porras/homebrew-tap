46e6c0a786fbeca269fb081404fc6363f59cc12a
Sergio Gil <sgilperez@gmail.com>
2016-01-05 01:40:10 +0100

    [tlcr] Add head

--- tlcr.rb
+++ tlcr.rb
@@ -3,6 +3,7 @@ class Tlcr < Formula
   homepage "https://github.com/porras/tlcr"
   url "https://github.com/porras/tlcr/archive/0.1.1.tar.gz"
   sha256 "6ed81377a751b2679e2052be6e88b9b15a5feda59eb9bb024a1eefbee7b99295"
+  head "https://github.com/porras/tlcr.git"
 
   bottle do
     root_url "https://github.com/porras/tlcr/releases/download/0.1.1"
