6e90aca8e2ce736ec2c344d118603aedc0c7b780
Sergio Gil <sgilperez@gmail.com>
2016-01-08 22:07:53 +0100

    [tlcr] v0.3.0

--- tlcr.rb
+++ tlcr.rb
@@ -1,13 +1,13 @@
 class Tlcr < Formula
   desc "Simple terminal-based client for TLDR pages, written in Crystal"
   homepage "https://github.com/porras/tlcr"
-  url "https://github.com/porras/tlcr/archive/0.2.1.tar.gz"
-  sha256 "224e3818abad044cd65330b06e88d4730cbcc2c1b90fba48aa2f55d3f59e37e6"
+  url "https://github.com/porras/tlcr/archive/0.3.0.tar.gz"
+  sha256 "d0f8be0b5d85fe0ed9587da4d32bdf7c8c401014bca83200aa8833f64d40b0c9"
   head "https://github.com/porras/tlcr.git"
 
   bottle do
-    root_url "https://github.com/porras/tlcr/releases/download/0.2.1"
-    sha256 "b7f7718aa6896e767b841129eeed5c008989ebfb31090fb0fa817cd90037c0d8" => :yosemite
+    root_url "https://github.com/porras/tlcr/releases/download/0.3.0"
+    sha256 "6f434710fa787ab90da18d27ac811d0307552c4753e5a6c679689ba8c7a38b1e" => :yosemite
   end
 
   depends_on "crystal-lang" => :build
