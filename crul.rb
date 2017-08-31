25631d49e8faa638e98308ac61cb148674aab386
Sergio Gil <sgilperez@gmail.com>
2017-08-31 22:51:29 +0200

    [crul] v0.4.2

--- crul.rb
+++ crul.rb
@@ -1,13 +1,13 @@
 class Crul < Formula
   desc "command-line HTTP client (alternative to curl) written in Crystal"
   homepage "https://github.com/porras/crul"
-  url "https://github.com/porras/crul/archive/v0.4.1.tar.gz"
-  sha256 "c9ef775560e04e7125cf8f6123846d563f09e4ae2e56e6d036694896521c57fd"
+  url "https://github.com/porras/crul/archive/v0.4.2.tar.gz"
+  sha256 "23ad4d583c61d3164e7e9bbe03fbea77b959ff14fc22a0c87910412bae2fb121"
   head "https://github.com/porras/crul.git"
 
   bottle do
-    root_url "https://github.com/porras/crul/releases/download/v0.4.1/"
-    sha256 "230777e48c3431a8639232c3a5087ee97d63209f3c95568e72208d294a00f7a9" => :el_capitan
+    root_url "https://github.com/porras/crul/releases/download/v0.4.2"
+    sha256 "6bf754dede6c9e1ba824b4cfb0bb82591713f8cf0dac56a75c85499c6e1516bb" => :el_capitan
   end
 
   depends_on "crystal-lang" => :build
